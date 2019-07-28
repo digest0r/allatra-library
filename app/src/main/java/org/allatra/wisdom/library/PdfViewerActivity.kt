b7846ceeb2354865e6adbfac872c6e5575168163
Vaclav Schmuttermeier <vaclav.schmuttermeier@gmail.com>
2019-07-28 15:02:38 +0200

    Added raw resources, Added layout for the reader.

--- app/src/main/java/org/allatra/wisdom/library/PdfViewerActivity.kt
+++ app/src/main/java/org/allatra/wisdom/library/PdfViewerActivity.kt
@@ -2,11 +2,19 @@ package org.allatra.wisdom.library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.barteksc.pdfviewer.PDFView
+import org.allatra.wisdom.library.R
 
 class PdfViewerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdf_viewer)
+
+        val pdfViewerComponent = findViewById<PDFView>(R.id.pdfView)
+        pdfViewerComponent?.let {
+            val inputStream = resources.openRawResource(R.raw.sensei_1_book_pdf_en)
+            it.fromStream(inputStream).load()
+        }
     }
 }
