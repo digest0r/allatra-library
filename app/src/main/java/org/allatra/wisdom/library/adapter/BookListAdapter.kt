9c90ee64c7445996913f666cddedb46a6b622999
Vaclav Schmuttermeier <vaclav.schmuttermeier@gmail.com>
2019-07-28 14:41:43 +0200

    Added new Pdfviewer activity.

--- app/src/main/java/org/allatra/wisdom/library/adapter/BookListAdapter.kt
+++ app/src/main/java/org/allatra/wisdom/library/adapter/BookListAdapter.kt
@@ -8,6 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import org.allatra.wisdom.library.R
 import org.allatra.wisdom.library.db.BookInfo
 import org.allatra.wisdom.library.view.BookListViewHolder
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import org.allatra.wisdom.library.PdfViewerActivity
 
 class BookListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -34,7 +37,13 @@ class BookListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         bookViewHolder.itemView.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View) {
                 Log.d(TAG, "Position clicked: $position, Book to open: ${listOfBooks[position].getTitle()}")
-                //TODO: Pass the book to the pdf reader
+
+                //Create an intent to pass previous user data
+                val context = bookViewHolder.itemView.context
+                val intent = Intent(context, PdfViewerActivity::class.java)
+                intent.putExtra("indexPage", listOfBooks[position].getIndexPage())
+                intent.putExtra("pdfBookName", listOfBooks[position].getPdfName())
+                startActivity(context, intent, intent.extras)
             }
         })
     }
